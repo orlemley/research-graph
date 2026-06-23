6b30d8f1ae705ff77652359b021f5de36485e4c4
orlemley <orlemley@gmail.com>
2026-06-22 21:19:25 -0500

    Reference table filtering
     - Added filtering for authors, concepts, institutins, sources
     - These filtered tables will be used for creating metrics tables and other things

--- src/research_graph/pipelines/graph_filtering_pipeline.py
+++ src/research_graph/pipelines/graph_filtering_pipeline.py
@@ -8,10 +8,29 @@ from research_graph.processing.graph_filtering import citation_edges_filter
 from research_graph.processing.graph_filtering import works_filter
 from research_graph.processing.graph_filtering import work_ids_filter
 from research_graph.processing.graph_filtering import relationship_tables_filter
+from research_graph.processing.graph_filtering import reference_tables_filter
 
 
 logger = logging.getLogger(__name__)
 
+REFERENCE_TABLES = {
+    "authors": {
+        "id_column": "author_id",
+        "base_table": "authorships"
+    },
+    "concepts": {
+        "id_column": "concept_id",
+        "base_table": "scores"
+    },
+    "institutions": {
+        "id_column": "institution_id",
+        "base_table": "affiliations"
+    },
+    "venues": {
+        "id_column": "venue_id",
+        "base_table": "works"
+    }
+}
 
 RELATIONSHIP_TABLES = ["affiliations", "authorships", "scores", "selected_scores"]
 
@@ -49,7 +68,7 @@ def run(context):
     try:
         temp_scores_done = False
         for file_number in range(scores_file_count):
-            if (filtered_work_ids_root / ".done").exists() or ((filtered_works_root / ".done").exists() and (filtered_tables_root / ".done").exists()):
+            if (filtered_work_ids_root / ".done").exists() or ((filtered_works_root / ".done").exists() and (filtered_tables_root / "relationship_tables.done").exists()):
                 temp_scores_done = True
                 break
             temp_scores_output_path = temp_scores_root / f"temp_scores_{file_number}.parquet"
@@ -84,7 +103,7 @@ def run(context):
             if (filtered_work_ids_root / ".done").exists():
                 shutil.rmtree(temp_scores_root)
 
-        elif (filtered_work_ids_root / ".done").exists() or ((filtered_works_root / ".done").exists() and (filtered_tables_root / ".done").exists()):
+        elif (filtered_work_ids_root / ".done").exists() or ((filtered_works_root / ".done").exists() and (filtered_tables_root / "relationship_tables.done").exists()):
             pass
 
         else:
@@ -107,7 +126,7 @@ def run(context):
                 (filtered_works_root / ".done").touch(exist_ok=True)
                 logger.info("Completed all works filtering")
 
-            table_files_exist = []
+            relationship_table_files_exist = []
 
             for table in RELATIONSHIP_TABLES:
                 sub_tables_root = tables_root / table
@@ -119,20 +138,45 @@ def run(context):
                     if not (sub_filtered_tables_root / f"filtered_{table}_{file_number}.parquet").exists():
                         relationship_tables_filter.filter_relationship_table(table, file_number, config, con)
 
-                    table_files_exist.append((sub_filtered_tables_root / f"filtered_{table}_{file_number}.parquet").exists())
+                    relationship_table_files_exist.append((sub_filtered_tables_root / f"filtered_{table}_{file_number}.parquet").exists())
             
-            if table_files_exist and all(table_files_exist) and (filtered_works_root / ".done").exists():
-                (filtered_tables_root / ".done").touch(exist_ok=True)
+            if relationship_table_files_exist and all(relationship_table_files_exist) and (filtered_works_root / ".done").exists():
+                (filtered_tables_root / "relationship_tables.done").touch(exist_ok=True)
                 shutil.rmtree(filtered_work_ids_root)
                 logger.info("Completed all relationship tables filtering")
 
-        elif ((filtered_works_root / ".done").exists() and (filtered_tables_root / ".done").exists()):
+        elif ((filtered_works_root / ".done").exists() and (filtered_tables_root / "relationship_tables.done").exists()):
             pass
         
         else:
             logger.error(f"Cannot filter works and relationship tables because filtered work ids aren't finished")
             raise RuntimeError(f"Cannot filter works and relationship tables because filtered work ids aren't finished")
 
+        reference_table_files_exist = []
+
+        if (filtered_tables_root / "relationship_tables.done").exists():
+            for name, values in REFERENCE_TABLES.items():
+                sub_tables_root = tables_root / name
+                sub_filtered_tables_root = filtered_tables_root / f"filtered_{name}"
+
+                table_file_count = len([file for file in sub_tables_root.glob("*.parquet")])
+
+                for file_number in range(table_file_count):
+                    if not (sub_filtered_tables_root / f"filtered_{name}_{file_number}.parquet").exists():
+                        reference_tables_filter.filter_reference_table(file_number, name, values, config, con)
+                    
+                    reference_table_files_exist.append((sub_filtered_tables_root / f"filtered_{name}_{file_number}.parquet").exists())
+
+            if reference_table_files_exist and all(reference_table_files_exist):
+                (filtered_tables_root / "reference_tables.done").touch(exist_ok=True)
+                logger.info("Completed all reference tables filtering")
+        
+        elif ((filtered_tables_root / "reference_tables.done").exists()):
+            pass
+
+        else:
+            logger.error(f"Cannot filter reference tables because filtered relationship tables aren't finished")
+            raise RuntimeError(f"Cannot filter reference tables because filtered relationship tables aren't finished")
 
     finally:
         con.close()
